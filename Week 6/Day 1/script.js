406d02ab47e43b969c57be0fc6acf2d5e430e7e1
AderemiGiuseppe <adetayobami@gmail.com>
2024-05-07 18:07:46 +0100

    Implemented monkey emotions toggling

--- Week 6/Day 1/script.js
+++ Week 6/Day 1/script.js
@@ -2,3 +2,14 @@ const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 
 // Add event listener here
+
+closedFace.addEventListener("click", () => {
+    console.log("closedFace is clicked");
+    closedFace.classList.add("hidden");
+    openFace.classList.remove("hidden");
+});
+openFace.addEventListener("click", () => {
+    console.log("openFace is clicked");
+    openFace.classList.add("hidden");
+    closedFace.classList.remove("hidden");
+});
